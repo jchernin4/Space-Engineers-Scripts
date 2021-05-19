b9a6d2e2491199a969d030652053de21a5a42437
jchernin4 <jeremychernin@gmail.com>
2021-05-19 08:47:02 -0400

    I dunno if this works

--- InventoryMonitor/Program.cs
+++ InventoryMonitor/Program.cs
@@ -26,6 +26,30 @@ namespace IngameScript {
 
 		public void Main(string argument, UpdateType updateSource) {
 			List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+			GridTerminalSystem.GetBlocks(blocks); // Get every connected block
+
+			// For storing item amounts
+			Dictionary<MyItemType, MyFixedPoint> totalBlocks = new Dictionary<MyItemType, MyFixedPoint>();
+			
+			// Check every block
+			foreach (IMyTerminalBlock block in blocks) {
+				// Check every inventory
+				for (int i = 0; i < block.InventoryCount; i++) {
+					IMyInventory curInv = block.GetInventory(i);
+
+					List <IMyInventoryItem> curItems = new List<IMyInventoryItem>();
+					for (int j = 0; j < curInv.Itemcount; j++) {
+						IMyInventoryItem item = curInv.GetItemAt(j);
+						
+						if (!totalBlocks.ContainsKey(item.Type)) {
+							totalBlocks.Add(item.Type, item.Amount);
+							
+						} else {
+							totalBlocks[item.Type] = totalBlocks[item.Type] + item.Amount;
+						}
+					}
+				}
+			}
 		}
 	}
 }
