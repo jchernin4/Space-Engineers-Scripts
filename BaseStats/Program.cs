cda0c024c6deb16621385067b45a833de393e734
jchernin4 <jeremychernin@gmail.com>
2023-01-22 17:47:58 -0500

    Update from 5/19/2021 project on PC

--- BaseStats/Program.cs
+++ BaseStats/Program.cs
@@ -28,9 +28,9 @@ namespace IngameScript {
 
         public void Main(string argument, UpdateType updateSource) {
             List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-            List<IMyTextPanel> lcds = new List<IMyTextPanel>();
             GridTerminalSystem.GetBlocks(blocks); // Get every connected block
 
+            List<IMyTextPanel> lcds = new List<IMyTextPanel>();
             List<IMyTextPanel> allLcds = new List<IMyTextPanel>();
             GridTerminalSystem.GetBlocksOfType(allLcds);
 
@@ -57,14 +57,25 @@ namespace IngameScript {
                 currentTotalOutput += producer.CurrentOutput;
                 maxTotalOutput += producer.MaxOutput;
             }
-            
+
+            float batteryStored = 0;
+            float batteryMax = 0;
+            List<IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();
+            GridTerminalSystem.GetBlocksOfType(batteries);
+            foreach (IMyBatteryBlock battery in batteries) {
+                batteryStored += battery.CurrentStoredPower;
+                batteryMax += battery.MaxStoredPower;
+            }
+
             foreach (IMyTextPanel lcd in lcds) {
                 lcd.WriteText("Base Stats\n==================================\n");
-                
-                lcd.WriteText("Connected Ships: " + numConnected + " / " + connectors.Count + "\n", true);
+                // numConnected includes the connectors of the ships that are connected, since they are technically on the same grid now, so you can't use connectors.Count
+                lcd.WriteText("Connected Ships: " + numConnected / 2 + "\n", true);
                 
                 lcd.WriteText("Power: " + Math.Round(currentTotalOutput * 1000, 2) + " / " + Math.Round(maxTotalOutput * 1000, 2) + "kW", true);
-                lcd.WriteText(" (" + Math.Round(currentTotalOutput / maxTotalOutput * 100, 2) + "% of max)", true);
+                lcd.WriteText(" (" + Math.Round(currentTotalOutput / maxTotalOutput * 100, 2) + "% of max)\n", true);
+                
+                lcd.WriteText("Battery: " + batteryStored * 1000 + "kw / " + batteryMax * 1000 + "kw (" + Math.Round(batteryStored / batteryMax * 100, 2) + "%)", true);
             }
         }
     }
---
+++ SolarPanelRotator/Program.cs
@@ -0,0 +1,72 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Sandbox.Game.Entities;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript {
+	partial class Program : MyGridProgram {
+		float curProduction = 0;
+		private string prefix = "[SPR]";
+		
+		public Program() {
+			Runtime.UpdateFrequency = UpdateFrequency.Update10; // Update every 10th tick
+		}
+
+		public void Save() {
+			Storage = curProduction.ToString();
+		}
+
+		public void Main(string argument, UpdateType updateSource) {
+			List<IMyTerminalBlock> allRotors = new List<IMyTerminalBlock>();
+			GridTerminalSystem.GetBlocksOfType<IMyMotorRotor>(allRotors);
+			List<IMySolarPanel> allPanels = new List<IMySolarPanel>();
+			GridTerminalSystem.GetBlocksOfType(allPanels);
+			
+			List<IMySolarPanel> panels = new List<IMySolarPanel>();
+			foreach (IMySolarPanel panel in allPanels) {
+				if (panel.CustomName.EndsWith(prefix)) {
+					panels.Add(panel);
+				}
+			}
+			
+			List<IMyTerminalBlock> rotors = new List<IMyTerminalBlock>();
+			foreach (IMyTerminalBlock rotor in allRotors) {
+				Echo("Display name: " + rotor.DisplayName);
+				if (rotor.DisplayName.EndsWith(prefix)) {
+					rotors.Add(rotor);
+				}
+			}
+
+			curProduction = 0;
+			foreach (IMySolarPanel panel in panels) {
+				curProduction += panel.CurrentOutput;
+			}
+
+			if (Storage != null && curProduction < float.Parse(Storage)) {
+				foreach (IMyTerminalBlock rotor in rotors) {
+					IMyMotorStator stator = (IMyMotorStator) rotor;
+					float angle = (stator.Angle / (float) Math.PI * 180f);
+					stator.SetValue<float>("UpperLimit", angle + 20);
+					stator.SetValue<float>("LowerLimit", angle - 20);
+					stator.SetValue<float>("Velocity", 3);
+				}
+			}
+		}
+	}
+}
